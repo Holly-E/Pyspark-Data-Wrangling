b14f597e8557e979070b66265a4d025b85291f51
Holly <Erickson.Holly@icloud.com>
2020-07-14 16:00:03 -0500

    initial commit

---
+++ DSC650_week10.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Erickson, Holly
+DSC650 WEEK 10
+
+1. 10.2 Programming Exercise: Fit a Binary Logistic Regression Model to a Dataset
+
+1. Build a Classification Model
+
+Predict the sex of a person based on their age, name, and state
+
+a. Prepare in Input Features
+"""
+
+from pyspark.sql import SparkSession
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer, VectorAssembler
+from pyspark.ml.feature import OneHotEncoderEstimator
+#%%
+spark = SparkSession.builder.appName("Week4").getOrCreate()
+
+csv_file_path = 'C://Master/Semester_5/baby-names.csv'
+         
+df = spark.read.load(
+  csv_file_path,
+  format="csv",
+  sep=",",
+  inferSchema=True,
+  header=True
+)
+
+df.printSchema()
+
+#%%
+
+# StringIndexer along with the OneHotEncoderEstimator to convert the name, state, and sex columns 
+#  Use the VectorAssembler to combine the name, state, and age vectors into a single features vector.
+
+categoricalColumns = ['name', 'state', 'sex']
+stages = []
+for categoricalCol in categoricalColumns:
+    stringIndexer = StringIndexer(inputCol = categoricalCol, outputCol = categoricalCol + 'Index')
+    encoder = OneHotEncoderEstimator(inputCols=[stringIndexer.getOutputCol()], outputCols=[categoricalCol + "classVec"])
+    stages += [stringIndexer, encoder]
+label_stringIdx = StringIndexer(inputCol = 'deposit', outputCol = 'label')
+stages += [label_stringIdx]
+numericCols = ['age', 'balance', 'duration', 'campaign', 'pdays', 'previous']
+assemblerInputs = [c + "classVec" for c in categoricalColumns] + numericCols
+assembler = VectorAssembler(inputCols=assemblerInputs, outputCol="features")
+stages += [assembler]
+
+
+indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(df) for column in list(set(df.columns)-set(['year', 'count'])) ]
+
+encoder = OneHotEncoderEstimator(
+    inputCols=[indexer.getOutputCol() for indexer in indexers],
+    outputCols=["{0}_encoded".format(indexer.getOutputCol()) for indexer in indexers]
+)
+
+assembler = VectorAssembler(
+    inputCols=['name_index_encoded', 'state_index_encoded', 'year'],
+    outputCol="features"
+)
+
+pipeline = Pipeline(stages=indexers + [encoder, assembler])
+df_r = pipeline.fit(df).transform(df)
+
+df_r.printSchema()
+
+#%%
+
+#Your final dataset should contain a column called features containing the prepared vector 
+#and a column called label containing the sex of the person.
+
+keep = ['features', 'sex_index']
+df_final = df_r.select([column for column in df_r.columns if column in keep])
+df_final = df_final.withColumnRenamed("sex_index","label")
+df_final.printSchema()
+
+#%%
+#2. Fit and Evaluate the Model
+#Fit the model as a logistic regression model with the following parameters.
+# LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8).
+from pyspark.ml.classification import LogisticRegression
+lr = LogisticRegression(featuresCol = 'features', labelCol = 'label', maxIter=10, regParam=0.3, elasticNetParam=0.8)
+lrModel = lr.fit(df_final)
+
+# Provide the area under the ROC curve for the model.
+trainingSummary = lrModel.summary
+print('Area Under ROC: ' + str(trainingSummary.areaUnderROC))
+
+
+"""
+Output
+Area Under ROC: 0.5
+"""
---
+++ PySpark_Example.py
@@ -0,0 +1,336 @@
+# -*- coding: utf-8 -*-
+"""
+Erickson, Holly
+DSC650 WEEK 4
+"""
+
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("Week4").getOrCreate()
+
+#%%
+"""
+1. As a first step, load both files into Spark and print the schemas
+Data located at https://github.com/bellevue-university/dsc650/tree/master/data
+"""
+df_flights = spark.read.parquet("C:/Master/Semester 5/data/domestic-flights/flights.parquet")
+df_airport_codes = spark.read.load(
+  "C:/Master/Semester 5/data/airport-codes/airport-codes.csv",
+  format="csv",
+  sep=",",
+  inferSchema=True,
+  header=True
+)
+
+df_flights.printSchema()
+df_airport_codes.printSchema()
+
+"""
+Output:
+
+root
+ |-- origin_airport_code: string (nullable = true)
+ |-- destination_airport_code: string (nullable = true)
+ |-- origin_city: string (nullable = true)
+ |-- destination_city: string (nullable = true)
+ |-- passengers: long (nullable = true)
+ |-- seats: long (nullable = true)
+ |-- flights: long (nullable = true)
+ |-- distance: double (nullable = true)
+ |-- origin_population: long (nullable = true)
+ |-- destination_population: long (nullable = true)
+ |-- flight_year: long (nullable = true)
+ |-- flight_month: long (nullable = true)
+ |-- __index_level_0__: long (nullable = true)
+
+root
+ |-- ident: string (nullable = true)
+ |-- type: string (nullable = true)
+ |-- name: string (nullable = true)
+ |-- elevation_ft: double (nullable = true)
+ |-- continent: string (nullable = true)
+ |-- iso_country: string (nullable = true)
+ |-- iso_region: string (nullable = true)
+ |-- municipality: string (nullable = true)
+ |-- gps_code: string (nullable = true)
+ |-- iata_code: string (nullable = true)
+ |-- local_code: string (nullable = true)
+ |-- coordinates: string (nullable = true)
+
+"""
+
+#%%
+"""
+a. Join the Data
+Left join the data keeping all data in df_flights
+Print the schema of the joined dataframe.
+"""
+joinType = "outer"
+df_join = df_flights.join(df_airport_codes, df_flights.origin_airport_code == df_airport_codes.iata_code, how = "left")
+df_join.printSchema()
+
+"""
+Output:
+    
+root
+ |-- origin_airport_code: string (nullable = true)
+ |-- destination_airport_code: string (nullable = true)
+ |-- origin_city: string (nullable = true)
+ |-- destination_city: string (nullable = true)
+ |-- passengers: long (nullable = true)
+ |-- seats: long (nullable = true)
+ |-- flights: long (nullable = true)
+ |-- distance: double (nullable = true)
+ |-- origin_population: long (nullable = true)
+ |-- destination_population: long (nullable = true)
+ |-- flight_year: long (nullable = true)
+ |-- flight_month: long (nullable = true)
+ |-- __index_level_0__: long (nullable = true)
+ |-- ident: string (nullable = true)
+ |-- type: string (nullable = true)
+ |-- name: string (nullable = true)
+ |-- elevation_ft: double (nullable = true)
+ |-- continent: string (nullable = true)
+ |-- iso_country: string (nullable = true)
+ |-- iso_region: string (nullable = true)
+ |-- municipality: string (nullable = true)
+ |-- gps_code: string (nullable = true)
+ |-- iata_code: string (nullable = true)
+ |-- local_code: string (nullable = true)
+ |-- coordinates: string (nullable = true)
+
+"""
+
+#%%
+"""
+b. Rename and Remove Columns
+"""
+remove = ["__index_level_0__", "ident", "local_code", "continent", "iso_country", "iata_code"]
+df_remove = df_join.select([column for column in df_join.columns if column not in remove])
+#print(df_join.columns)
+#print(df_remove.columns)
+
+df_renamed = df_remove.withColumnRenamed("type","origin_airport_type") \
+    .withColumnRenamed("name", "origin_airport_name").withColumnRenamed( "elevation_ft", "origin_airport_elevation_ft") \
+        .withColumnRenamed("iso_region", "origin_airport_region").withColumnRenamed("municipality", "origin_airport_municipality") \
+        .withColumnRenamed("gps_code", "origin_airport_gps_code").withColumnRenamed( "coordinates", "origin_airport_coordinates")
+df_renamed.printSchema()
+
+"""
+Output:
+    
+root
+ |-- origin_airport_code: string (nullable = true)
+ |-- destination_airport_code: string (nullable = true)
+ |-- origin_city: string (nullable = true)
+ |-- destination_city: string (nullable = true)
+ |-- passengers: long (nullable = true)
+ |-- seats: long (nullable = true)
+ |-- flights: long (nullable = true)
+ |-- distance: double (nullable = true)
+ |-- origin_population: long (nullable = true)
+ |-- destination_population: long (nullable = true)
+ |-- flight_year: long (nullable = true)
+ |-- flight_month: long (nullable = true)
+ |-- origin_airport_type: string (nullable = true)
+ |-- origin_airport_name: string (nullable = true)
+ |-- origin_airport_elevation_ft: double (nullable = true)
+ |-- origin_airport_region: string (nullable = true)
+ |-- origin_airport_municipality: string (nullable = true)
+ |-- origin_airport_gps_code: string (nullable = true)
+ |-- origin_airport_coordinates: string (nullable = true)
+
+
+
+"""
+
+#%%
+"""
+c. Join to Destination Airport
+Join the airport codes file to the destination airport
+Drop the same columns and rename the same columns using the prefix destination_airport_ instead of origin_airport_
+The final schema and dataframe should contain the added information for the destination and origin airports.
+"""
+
+df_join2 = df_renamed.join(df_airport_codes, df_flights.destination_airport_code == df_airport_codes.iata_code, how = "left")
+#df_join2.printSchema()
+df_remove2 = df_join2.select([column for column in df_join2.columns if column not in remove])
+#print(df_join2.columns)
+
+df_renamed2 = df_remove2.withColumnRenamed("type","destination_airport_type") \
+    .withColumnRenamed("name", "destination_airport_name").withColumnRenamed( "elevation_ft", "destination_airport_elevation_ft") \
+        .withColumnRenamed("iso_region", "destination_airport_region").withColumnRenamed("municipality", "destination_airport_municipality") \
+        .withColumnRenamed("gps_code", "destination_airport_gps_code").withColumnRenamed( "coordinates", "destination_airport_coordinates")
+
+#print(df_flights.count())
+#print(df_renamed2.count())
+df_renamed2.printSchema()
+
+"""
+Output:
+    
+root
+ |-- origin_airport_code: string (nullable = true)
+ |-- destination_airport_code: string (nullable = true)
+ |-- origin_city: string (nullable = true)
+ |-- destination_city: string (nullable = true)
+ |-- passengers: long (nullable = true)
+ |-- seats: long (nullable = true)
+ |-- flights: long (nullable = true)
+ |-- distance: double (nullable = true)
+ |-- origin_population: long (nullable = true)
+ |-- destination_population: long (nullable = true)
+ |-- flight_year: long (nullable = true)
+ |-- flight_month: long (nullable = true)
+ |-- origin_airport_type: string (nullable = true)
+ |-- origin_airport_name: string (nullable = true)
+ |-- origin_airport_elevation_ft: double (nullable = true)
+ |-- origin_airport_region: string (nullable = true)
+ |-- origin_airport_municipality: string (nullable = true)
+ |-- origin_airport_gps_code: string (nullable = true)
+ |-- origin_airport_coordinates: string (nullable = true)
+ |-- id: long (nullable = false)
+ |-- destination_airport_type: string (nullable = true)
+ |-- destination_airport_name: string (nullable = true)
+ |-- destination_airport_elevation_ft: double (nullable = true)
+ |-- destination_airport_region: string (nullable = true)
+ |-- destination_airport_municipality: string (nullable = true)
+ |-- destination_airport_gps_code: string (nullable = true)
+ |-- destination_airport_coordinates: string (nullable = true)
+
+"""
+
+#%%
+"""
+d.Create a dataframe using only data from 2008. 
+This dataframe will be a report of the top ten airports by the number of inbound passengers (destination)
+This dataframe should contain specific fields
+Show the results of this dataframe using the show method.
+"""
+from pyspark.sql import Window
+from pyspark.sql.functions import col, row_number
+
+# Get top 10 destination airports for 2008
+df_2008 = df_renamed2.filter(df_renamed2.flight_year.contains('2008')) 
+df_grouped = df_2008.groupBy("destination_airport_code").sum("passengers")
+df_sorted = df_grouped.sort("sum(passengers)", ascending=False)
+df_top10 = df_sorted.limit(10)
+df_top10_alldata = df_top10.join(df_2008,"destination_airport_code", how = "left")
+
+# Calculate requested data and join to top 10 dataframe 
+df_top10 = df_top10.select('destination_airport_code','sum(passengers)', row_number().over(Window.partitionBy().orderBy(col('sum(passengers)').desc()) ).alias("Rank (1-10)"))
+df_total_flights = df_top10_alldata.groupBy("destination_airport_code").count()
+df_avg_flights = df_top10_alldata.groupBy("destination_airport_code").agg({'flights': 'mean'}) 
+df_top10 = df_top10.select('*', (df_top10['sum(passengers)'] / 365).alias("Average Daily Passengers"))       
+
+df_top10 = df_top10.join(df_avg_flights,"destination_airport_code", how = "left")
+df_top10 = df_top10.join(df_total_flights ,"destination_airport_code", how = "left")                     
+df_top10_Name = df_top10.join(df_airport_codes, df_top10.destination_airport_code == df_airport_codes.iata_code, how = "left")
+
+# Rename columns and show final data
+df_top10 = df_top10_Name.withColumnRenamed("sum(passengers)","Total Inbound Passengers") \
+    .withColumnRenamed("avg(flights)","Average Inbound Flights").withColumnRenamed("destination_airport_code", "IATA code") \
+        .withColumnRenamed("count","Total Inbound Flights").withColumnRenamed("name", "Name")
+df_final = df_top10.select("Rank (1-10)","Name", "IATA code","Total Inbound Passengers","Total Inbound Flights", "Average Daily Passengers", "Average Inbound Flights")
+df_final.show()
+
+"""
+Output:
+
++-----------+--------------------+---------+------------------------+---------------------+------------------------+-----------------------+
+|Rank (1-10)|                Name|IATA code|Total Inbound Passengers|Total Inbound Flights|Average Daily Passengers|Average Inbound Flights|
++-----------+--------------------+---------+------------------------+---------------------+------------------------+-----------------------+
+|          8|George Bush Inter...|      IAH|                14870717|                 5811|        40741.6904109589|      36.86886938564791|
+|          5|McCarran Internat...|      LAS|                18262263|                 3887|       50033.59726027397|      42.22356573192694|
+|          7|Charlotte Douglas...|      CLT|                15038489|                 6152|        41201.3397260274|      33.32899869960988|
+|          6|Phoenix Sky Harbo...|      PHX|                17305718|                 4423|       47412.92602739726|     40.981008365362875|
+|          3|Dallas Fort Worth...|      DFW|                22883558|                 5109|       62694.67945205479|      52.89547856723429|
+|          1|Hartsfield Jackso...|      ATL|                35561795|                 8775|       97429.57534246576|      45.03612535612535|
+|          2|Chicago O'Hare In...|      ORD|                26398793|                 9479|        72325.4602739726|      37.61683721911594|
+|         10|Detroit Metropoli...|      DTW|                14228887|                 5638|      38983.252054794524|      34.03866619368571|
+|          4|Los Angeles Inter...|      LAX|                19741782|                 4634|       54087.07397260274|      46.39620198532585|
+|          9|Orlando Internati...|      MCO|                14581086|                 3584|      39948.180821917806|     36.749441964285715|
++-----------+--------------------+---------+------------------------+---------------------+------------------------+-----------------------+
+"""
+
+#%%
+"""
+e. User Defined Functions
+Add new columns for destination_airport_longitude, destination_airport_latitude, origin_airport_longitude, and origin_airport_latitude.
+"""
+from pyspark.sql.functions import udf
+
+@udf('double')
+def get_latitude(coordinates):
+    split_coords = coordinates.split(',')
+    if len(split_coords) != 2:
+        return None
+
+    return float(split_coords[0].strip())
+
+
+@udf('double')
+def get_longitude(coordinates):
+    split_coords = coordinates.split(',')
+    if len(split_coords) != 2:
+        return None
+
+    return float(split_coords[1].strip())
+
+df_renamed2 = df_renamed2.withColumn(
+  'destination_airport_longitude',
+  get_longitude(df_renamed2['destination_airport_coordinates'])
+)
+
+df_renamed2  = df_renamed2.withColumn(
+  'destination_airport_latitude',
+  get_latitude(df_renamed2['destination_airport_coordinates'])
+)
+
+df_renamed2 = df_renamed2.withColumn(
+  'origin_airport_longitude',
+  get_longitude(df_renamed2['origin_airport_coordinates'])
+)
+
+df_renamed2 = df_renamed2.withColumn(
+  'origin_airport_latitude',
+  get_latitude(df_renamed2['origin_airport_coordinates'])
+)
+
+df_renamed2.printSchema()
+
+"""
+Output:
+    
+root
+ |-- origin_airport_code: string (nullable = true)
+ |-- destination_airport_code: string (nullable = true)
+ |-- origin_city: string (nullable = true)
+ |-- destination_city: string (nullable = true)
+ |-- passengers: long (nullable = true)
+ |-- seats: long (nullable = true)
+ |-- flights: long (nullable = true)
+ |-- distance: double (nullable = true)
+ |-- origin_population: long (nullable = true)
+ |-- destination_population: long (nullable = true)
+ |-- flight_year: long (nullable = true)
+ |-- flight_month: long (nullable = true)
+ |-- origin_airport_type: string (nullable = true)
+ |-- origin_airport_name: string (nullable = true)
+ |-- origin_airport_elevation_ft: double (nullable = true)
+ |-- origin_airport_region: string (nullable = true)
+ |-- origin_airport_municipality: string (nullable = true)
+ |-- origin_airport_gps_code: string (nullable = true)
+ |-- origin_airport_coordinates: string (nullable = true)
+ |-- destination_airport_type: string (nullable = true)
+ |-- destination_airport_name: string (nullable = true)
+ |-- destination_airport_elevation_ft: double (nullable = true)
+ |-- destination_airport_region: string (nullable = true)
+ |-- destination_airport_municipality: string (nullable = true)
+ |-- destination_airport_gps_code: string (nullable = true)
+ |-- destination_airport_coordinates: string (nullable = true)
+ |-- destination_airport_longitude: double (nullable = true)
+ |-- destination_airport_latitude: double (nullable = true)
+ |-- origin_airport_longitude: double (nullable = true)
+ |-- origin_airport_latitude: double (nullable = true)
+
+"""
